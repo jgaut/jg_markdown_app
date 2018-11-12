ebde4048f42363f3039f243ab45600463e8c39e8
Jérémy Gautier <jgautier@adista.fr>
2018-11-12 14:24:58 +0100

    first commit

---
+++ appserver/static/visualizations/jg_markdown/src/visualization_source.js
@@ -0,0 +1,66 @@
+/*
+ * Visualization source
+ */
+define([
+            'jquery',
+            'underscore',
+            'api/SplunkVisualizationBase',
+            'api/SplunkVisualizationUtils',
+            // Add required assets to this list
+            'showdown'
+        ],
+        function(
+            $,
+            _,
+            SplunkVisualizationBase,
+            vizUtils,
+            showdown
+        ) {
+  
+    // Extend from SplunkVisualizationBase
+    return SplunkVisualizationBase.extend({
+  
+        initialize: function() {
+            SplunkVisualizationBase.prototype.initialize.apply(this, arguments);
+	    $el = $(this.el);
+	    $.get( window.location.origin + "/static/app/jg_markdown_app/README.md", function( data ) {
+            	var converter = new showdown.Converter();
+	        converter.setFlavor('github');
+                var html = converter.makeHtml(data);
+           	$el.empty();
+	        $el.append(html);
+	    });
+
+            // Initialization logic goes here
+        },
+
+        // Optionally implement to format data returned from search. 
+        // The returned object will be passed to updateView as 'data'
+        formatData: function(data) {
+
+            // Format data 
+
+            return data;
+        },
+  
+        // Implement updateView to render a visualization.
+        //  'data' will be the data object returned from formatData or from the search
+        //  'config' will be the configuration property object
+        updateView: function(data, config) {
+            
+            // Draw something here
+
+        },
+
+        // Search data params
+        getInitialDataParams: function() {
+            return ({
+                outputMode: SplunkVisualizationBase.ROW_MAJOR_OUTPUT_MODE,
+                count: 10000
+            });
+        },
+
+        // Override to respond to re-sizing events
+        reflow: function() {}
+    });
+});
